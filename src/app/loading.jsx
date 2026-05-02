719929ecc81c9f128c7655fbd77d2cb84c8dce99
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-05-03 00:30:45 +0600

    lodaing updated

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='h-screen flex items-center justify-center'>
+           <span className="loading loading-bars loading-xl w-30 h-20"></span>
+        </div>
+    );
+};
+
+export default loading;
